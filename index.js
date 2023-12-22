f45ac0e09d8fb4f686f33facf2e0d2e56fde543c
Katharinaad <vermilion.adams@gmail.com>
2023-12-22 09:10:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const contacts = require("./contacts");
 const { Command } = require("commander");
+
 const program = new Command();
 program.option("-a, --action <type>", "choose action").option("-i, --id <type>", "user id").option("-n, --name <type>", "user name").option("-e, --email <type>", "user email").option("-p, --phone <type>", "user phone");
 
@@ -7,27 +9,31 @@ program.parse(process.argv);
 const argv = program.opts();
 
 // TODO: refactor
-function invokeAction({ action, id, name, email, phone }) {
+const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
-      // ...
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
       break;
 
     case "get":
-      // ... id
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
       break;
 
     case "add":
-      // ... name email phone
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
       break;
 
     case "remove":
-      // ... id
+      const removeContact = await contacts.removeContact(id);
+      console.log(removeContact);
       break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
-}
+};
 
 invokeAction(argv);
