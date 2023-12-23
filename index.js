7fbb33246ec051f99ed8efe9325c5b9474632493
Katharinaad <vermilion.adams@gmail.com>
2023-12-23 13:23:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,23 +12,15 @@ const argv = program.opts();
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
-      const allContacts = await contacts.listContacts();
-      console.table(allContacts);
       break;
 
     case "get":
-      const oneContact = await contacts.getContactById(id);
-      console.log(oneContact);
       break;
 
     case "add":
-      const newContact = await contacts.addContact(name, email, phone);
-      console.log(newContact);
       break;
 
     case "remove":
-      const removeContact = await contacts.removeContact(id);
-      console.log(removeContact);
       break;
 
     default:
