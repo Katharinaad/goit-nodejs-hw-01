a30eb1cb56e86b369768389d54c0a071a7dde0d1
Katharinaad <vermilion.adams@gmail.com>
2023-12-23 13:23:01 +0100

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,21 +1,54 @@
 const fs = require("fs/promises");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 // create an absolute path to contacts.json
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
+//generates unique identifier
+function generateUniqueId() {
+  return uuidv4();
+}
+
+// read contacts from the file
 async function listContacts() {
   const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
 }
 
-function getContactById(contactId) {
-  // ...your code
+// write an array of contact objects to a file, to a JSON file
+async function writeContacts(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 }
 
-function removeContact(contactId) {
-  // ...your code
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  return contacts.find((contact) => contact.id === contactId);
 }
 
-function addContact(name, email, phone) {
-  // ...your code
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const filteredContacts = contacts.filter((contact) => contact.id !== contactId);
+  await writeContacts(filteredContacts);
 }
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: generateUniqueId(),
+    name,
+    email,
+    phone,
+  };
+
+  contacts.push(newContact);
+  await writeContacts(contacts);
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
