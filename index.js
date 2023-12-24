cf25ccbbf48829d96433d1c07d39393c739bec13
Katharinaad <vermilion.adams@gmail.com>
2023-12-24 09:35:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Command } = require("commander");
+const contactsControls = require("./contacts");
 
 const program = new Command();
 program.option("-a, --action <type>", "choose action").option("-i, --id <type>", "user id").option("-n, --name <type>", "user name").option("-e, --email <type>", "user email").option("-p, --phone <type>", "user phone");
@@ -11,15 +12,19 @@ const argv = program.opts();
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
+      console.table(await contactsControls.listContacts());
       break;
 
     case "get":
+      console.table(await contactsControls.getContactById(id));
       break;
 
     case "add":
+      console.table(await contactsControls.addContact(name, email, phone));
       break;
 
     case "remove":
+      console.table(await contactsControls.removeContact(id));
       break;
 
     default:
